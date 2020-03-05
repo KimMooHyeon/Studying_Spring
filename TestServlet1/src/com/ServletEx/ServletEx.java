006af0797cc68dbd84b3573af0c601cf1d5b0335
KimMooHyeon <dlfb77@gmail.com>
2020-03-05 10:31:14 +0900

    Init PJT

---
+++ TestServlet1/src/com/ServletEx/ServletEx.java
@@ -0,0 +1,49 @@
+package com.ServletEx;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletEx
+ */
+
+public class ServletEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String adminId = getServletConfig().getInitParameter("adminId");
+		String adminPw = getServletConfig().getInitParameter("adminPw");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<p> adminId : " + adminId + "</p>");
+		out.print("<p> adminPw : " + adminPw + "</p>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
