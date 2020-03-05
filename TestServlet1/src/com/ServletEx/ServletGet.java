52f98ca15f9021a5af6de59792fec277d82bcb1e
KimMooHyeon <dlfb77@gmail.com>
2020-03-05 13:18:14 +0900

    Making Cookies and Login page

--- TestServlet1/src/com/ServletEx/ServletGet.java
+++ TestServlet1/src/com/ServletEx/ServletGet.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ServletEx
+ * Servlet implementation class ServletGet
  */
-
-public class ServletEx extends HttpServlet {
+@WebServlet("/ServletGet")
+public class ServletGet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletEx() {
+    public ServletGet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,23 +28,14 @@ public class ServletEx extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String adminId = getServletConfig().getInitParameter("adminId");
-		String adminPw = getServletConfig().getInitParameter("adminPw");
-		
-		PrintWriter out = response.getWriter();
-		out.print("<p> adminId : " + adminId + "</p>");
-		out.print("<p> adminPw : " + adminPw + "</p>");
-		
-		String imgDir = getServletContext().getInitParameter("imgDir");
-		String testServerIP = getServletContext().getInitParameter("testServerIP");
 	
-		out.print("<p> imgDir : " + imgDir + "</p>");
-		out.print("<p> testServerIP : " + testServerIP + "</p>");
-		
-		getServletContext().setAttribute("connectedIP","165.62.58.33");
-		getServletContext().setAttribute("connectedUser","MooHyeon");
-		
+			String connectedIP = (String)getServletContext().getAttribute("connectedIP");
+			String connectedUser = (String)getServletContext().getAttribute("connectedUser");
+			
+			PrintWriter out = response.getWriter();
+			out.print("<p> connectedIP : " + connectedIP + "</p>");
+			out.print("<p> connectedUser : " + connectedUser + "</p>");
+			
 	}
 
 	/**
